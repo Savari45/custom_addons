a53470a0afb87e91b623d904eb9f39a6dab4e9e0
sarath-ksa <saraathkumar21@gmail.com>
2025-04-15 18:11:00 +0530

    first commit

--- customize_on_greenish/__manifest__.py
+++ customize_on_greenish/__manifest__.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name': 'Greenish',
+    'name': 'MRP in Invoices',
+    'version': '18.0.1.0.1',
     'description': 'Adds Expiry Dates and Lots and Serial into Invoice/Bill Move Lines from its related stock moves',
     'sequence': 1,
-    'author': 'Alan',
-    'category': 'Accounting/Accounting',
-    'summary': 'Adds Expiry Dates and Lots into Invoice/Bill Move Lines from its related stock moves',
-    'version': '1.0',
+    'author': 'Alan Technologies',
+    'company': 'Alan Technologies',
+    'maintainer': 'Alan Technologies',
+    'website': "https://alantechnologies.in/",
+    'category': 'Invoice',
+    'summary': 'Add the MRP in the Product and Incoice Lines ',
     'depends': ['base', 'stock', 'sale', 'account', 'purchase'],
     'data': [
         # 'security/ir.model.access.csv',
@@ -17,10 +20,13 @@
         'views/sale_order_line.xml',
         'report/inherit_invoice_report.xml',
         'actions/dynamic_cash_rounding.xml',
+        'report/custom_header.xml',
 
         # 'report/paper_format.xml',
     ],
+    'images': ['static/description/banner.gif'],
     'installable': True,
     'application': True,
-    'license': 'LGPL-3',
+    'auto_install': False,
+    'license': 'AGPL-3',
 }
