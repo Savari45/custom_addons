4966f2b90e0a116e1473e49a49616ce0cc5a525b
savari45 <savariuma045@gmail.com>
2025-04-24 11:23:43 +0530

    add the module Quotation_send_Excel

--- quotation_send_excel/__manifest__.py
+++ quotation_send_excel/__manifest__.py
@@ -1,23 +1,30 @@
 {
-    'name': "sale send Quotation in Excel Sheets ",
+    'name': "Send Sale Quotation as Excel via Email ",
     'description': """
-        Hospital management module which is used to mange the hospital functionalities prescription,patient,doctor diagnosis etc
+        This module allows users to generate Excel sheets for Sale Quotations and send them as email attachments directly from the Odoo interface.
+
+        Key Features:
+        - Generate Excel (.xlsx) version of a sale quotation
+        - Include product lines, customer details, totals, and more
+        - Send email with Excel attachment using a email template
+        - Log email activity in chatter for reference
+        - User access control for sending Excel quotations
+
     """,
-    'summary': """
-    Hospital management module which is used to mange the hospital functionalities prescription,patient,doctor diagnosis etc
-""",
-    'author': "Alan Technologies",
-    'company': "Alan Technologies",
+    'summary': """ Automatically generate and send Sale Quotations as Excel attachments via email.""",
+    'sequence': 1,
+    'author': 'Alan Technologies',
+    'company': 'Alan Technologies',
     'maintainer': 'Alan Technologies',
-    'website': "https://www.alantech.com",
+    'website': "https://alantechnologies.in/",
     "license": "AGPL-3",
     'category': 'sale',
     'version': '18.0.1.0.0',
-    'depends': ['base', 'hr', 'account', 'sale', 'product', 'web','stock'],
+    'depends': ['base', 'sale'],
     'data': [
 
     ],
-
+    'images': ['static/description/banner.gif'],
     'installable': True,
     'auto_install': False,
     'application': True,
